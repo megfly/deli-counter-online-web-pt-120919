555b9350c2b4a7559b18d185506938dcc8434cff
Meghan Flynn <mflynn333@gmail.com>
2019-12-17 00:54:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 def line(katz_deli)
-  if katz_deli == 0 
+  if katz_deli.empty?
     puts "The line is currently empty."
   else 
-    customer_position =[] 
+    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}" 
   end
 end 
 
