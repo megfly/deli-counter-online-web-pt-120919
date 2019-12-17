c38706d6294ec20e69b7cf0857c1b45967ada829
Meghan Flynn <mflynn333@gmail.com>
2019-12-17 01:02:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,11 @@ def take_a_number(katz_deli, name)
 end 
 
 
-def now_serving
+def now_serving(katz_deli)
+  if katz_deli !=[]
+    puts "Currently serving #{katz_deli.first}"
+  else 
+    puts "There is nobody waiting to be served!"
+  end
+  katz_deli.shift
 end
