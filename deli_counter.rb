dde4ff38a2c7a69f4ca183dacdb57814f810869e
Meghan Flynn <mflynn333@gmail.com>
2019-12-17 01:13:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,14 @@
 def line(katz_deli)
-  if katz_deli.empty?
+  katz_deli = []
   
-end 
+  if katz_deli == 0 
+    puts "The line is currently empty."
+  else 
+    current_line = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      current_line.concat " #{index+1}. #{name}"
+  end 
+  puts current_line
 end 
 
 
