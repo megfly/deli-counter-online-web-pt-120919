f2870e79e47fc52439edf89093f6211b78be854e
Meghan Flynn <mflynn333@gmail.com>
2019-12-17 01:28:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,15 @@
 def line(katz_deli)
+  line_position =[]
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else 
+    number = 1 
+    katz_deli.each do |customer|
+      line_position << "#{number}. #{customer}"
+      number +=1 
+    end 
+    puts "The line is currently: #{line_position.join(" ")}"
+  end 
 end 
 
 
