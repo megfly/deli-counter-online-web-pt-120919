dfb4c5712ff105e82b5bbbcb27bf2e28ae55e9aa
Meghan Flynn <mflynn333@gmail.com>
2019-12-17 01:29:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,10 +14,8 @@ end
 
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  position = katz_deli.index(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
- 
+  katz_deli << name 
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end 
 
 
